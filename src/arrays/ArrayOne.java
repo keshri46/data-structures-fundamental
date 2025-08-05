8427429a8fefd4dd1758d25aa62fe5bfa14b23ee
keshri46 <keshri1613@gmail.com>
2025-08-05 14:30:29 +0530

    welcome back array

--- src/arrays/ArrayOne.java
+++ src/arrays/ArrayOne.java
@@ -1,4 +1,23 @@
 package arrays;
 
 class ArrayOne {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        int[] b = {1,3,5,7};
+
+        for (int j : a) {
+            System.out.println(j);
+        }
+        for (int j : b) {
+            System.out.println(j);
+        }
+        /*
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+         */
+    }
 }
